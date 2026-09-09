1678dbcd3f31b8db57eb89279700bc983cac604e
mhcampos1 <mhcampos@lbl.gov>
2026-09-08 17:48:38 -0700

    Created sequence_insitu_pulse_reaction

---
+++ sequence_insitu_pulse_reaction.py
@@ -0,0 +1,144 @@
+"""
+Created on Jul 23, 2026
+
+@author: 
+"""
+
+import pyqtgraph as pg
+from qtpy import QtCore, QtWidgets
+
+from ScopeFoundry import Measurement, h5_io
+# from ScopeFoundryHW.keithley_sourcemeter2400.sourcemeter2400_dev import Sourcemeter2400Dev
+
+import numpy as np
+import time
+
+class SequenceInsituPulseReaction(Measurement):
+
+    name = "sequence_insitu_pulse_reaction"
+
+    class SignalWorker(QtCore.QObject):
+        """
+        Class to handle Qt.Signals. 
+        """
+        # Signal to update the progess bar
+        update_progress = QtCore.Signal(float)
+
+        def __init__(self, parent=None):
+            super().__init__(parent)
+
+    def setup(self):
+        """
+        Runs once during app initialization. This is where you define your 
+        settings, and set up data structures. This is an inherited function.
+        """
+        s = self.settings
+
+        # Voltage sweeping range
+        self.voltage_range = s.New_Range(
+            "Voltage Range", initials = [0.5, 3, 0.1], unit = "V", si=True, 
+            vmin = -20, vmax = 20,
+            description = "Voltage sweep range."
+        )
+        
+        # Initialize the signal worker
+        self.sig_worker = self.SignalWorker()
+
+    def setup_figure(self):
+        """
+        Runs once during app initialization and is responsible
+        for creating a QtWidgets.QWidget self.ui. This is an inherited class
+        from the Measurement class.
+        """
+
+        # ----- Measurement Control Board (cb)
+        cb_layout = QtWidgets.QVBoxLayout()
+        
+        # Voltage range (vrng) Layout
+        vrng_layout=QtWidgets.QVBoxLayout()
+        vrng_layout.addWidget(
+            self.settings.New_UI(
+                include = ("Voltage Range_min","Voltage Range_max",
+                           "Voltage Range_step", "Voltage Range_num"),
+                title="Voltage Sweep",
+            )
+        )
+
+        cb_layout.addLayout(vrng_layout)
+
+        # Run Setting Layout
+        run_layout=QtWidgets.QVBoxLayout()
+        run_layout.addWidget(
+            self.settings.New_UI(
+                include = ("progress",),
+            )
+        )
+        run_layout.addWidget(self.new_start_stop_button())
+        
+        cb_layout.addLayout(run_layout)
+
+        header_widget = QtWidgets.QWidget()
+        header_layout = QtWidgets.QVBoxLayout(header_widget)
+        header_layout.addLayout(cb_layout)
+
+        # ScopeFoundry assumes .ui is the main widget:
+        self.ui = QtWidgets.QSplitter(QtCore.Qt.Orientation.Vertical)
+        self.ui.addWidget(header_widget)
+
+    def pre_run(self):
+        """
+        Runs right before the measurement starts. This is an inherited function
+        from the Measurement class.
+        """
+        # Connect the signals to display update functions and progress update        
+        self.sig_worker.update_progress.connect(
+            self.set_progress,
+            QtCore.Qt.QueuedConnection
+        )
+
+        # Check debug boolean
+        self.debug = self.app.hardware["sourcemeter2400"].dev.debug
+
+        self.insitu_pulse_reaction = self.app.measurements[
+            'insitu_pulse_reaction_readout'
+        ]
+    
+    def run(self):
+        """
+        Runs when the measurement starts. Executes in a separate thread from the GUI.
+        It should not update the graphical interface directly and should focus only
+        on data acquisition.
+        """
+
+        print("\n************************************")
+        print("\nBEGINNING SEQUENTIAL INSITU REACTION")
+        print("\n************************************")
+
+
+        # Make sure the data will save
+        self.insitu_pulse_reaction.settings["save_h5"] = True
+
+        # Sweep the voltage setpoints and measure the current
+        N = len(self.voltage_range.sweep_array)
+        for i, V in enumerate(self.voltage_range.sweep_array):
+            self.insitu_pulse_reaction.settings["Pulse DC Voltage"] = V
+
+            # Begin the measurement and record if it is completed successfully
+            measurement_success = self.start_nested_measure_and_wait(
+                self.insitu_pulse_reaction, nested_interrupt = False
+            )
+
+            # Signal to update the progress bar
+            self.sig_worker.update_progress.emit((i+1) * 100.0 / N)
+            print(f"Experiment # {i+1}: {V:.2e} V")
+
+            # If the last measurement was interrupted or unsuccessful end
+            if not measurement_success:
+                break
+
+    # def post_run(self):
+    #     """
+    #     Inherited function that runs after the measurement run is interrupted 
+    #     or is completed.
+    #     """
+    #     # In case of interruption try to turn off the output
